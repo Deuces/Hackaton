09fb43852f8f6f992d73c59721da247a72aff9d1
sanchousese <saniasutula@gmail.com>
2014-11-29 17:57:09 +0200

    Database was proected and created

--- GlobalRating/__init__.py
+++ GlobalRating/__init__.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
-import GlobalRating.view
+from GlobalRating import view, models
--- GlobalRating/models.py
+++ GlobalRating/models.py
@@ -1,5 +1,27 @@
 from GlobalRating import db
 
-class User(db.Model) :
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(64), index = True, unique = True)
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+    ratings = db.relationship('Rating', backref='author', lazy='dynamic')
+
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    mark = db.Column(db.Integer, index=True)
+    usr_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cat_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    type = db.Column(db.String(64), index=True, unique=True)
+    parent_id = db.Column(db.Integer, index=True, nullable=True)
+    address = db.Column(db.String(64), index=True, nullable=True)
