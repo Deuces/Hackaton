2e64061a7416fe4392f98710c0f6814893d5a7ee
ScreaD <pavlosevidov@gmail.com>
2014-11-30 01:24:27 +0200

    item-style.css added

--- fillDB.py
+++ fillDB.py
@@ -2,12 +2,12 @@ from GlobalRating import db
 from GlobalRating.dbAPI import *
 from GlobalRating.models import User, Rating, Category
 
-# add_category("NTUU", "Very cool university", "university")
-# add_category("NAU", "Very low university", "university")
-# add_category("KNUTD", "Not Very cool university", "university")
+add_category("NTUU", "Very cool university", "university")
+add_category("NAU", "Very low university", "university")
+add_category("KNUTD", "Not Very cool university", "university")
 
-# add_user("Gogli", "dfdsadg")
-# rate_category(2, 2, 5)
+add_user("Gogli", "dfdsadg")
+rate_category(1, 2, 5)
 
 
 cat = get_category(1)
